86db2c1cefad041a9bc6bdca0717ab02e366b929
João Pedro Sconetto <sconetto.joao@gmail.com>
2018-08-21 16:36:16 -0300

    Finishing the 2nd exercise
    Creating other SQLs
    
    Changing the name and other minor updates

--- exercicios/aula1exer2/aula1exer2Fisico_JoaoPedroSconetto_14-0145940.sql
+++ exercicios/aula1exer2/aula1exer2Fisico_JoaoPedroSconetto_14-0145940.sql
@@ -1,4 +1,20 @@
-/* aula1exer2Logico_JoaoPedroSconetto_14-0145940: */
+-- ------           AULA 1 EXER 2            ---------- --
+--                                                      --
+--                    SCRIPT DE CRIAÇÃO (DDL)           --
+--                                                      --
+-- Data Criacao ...........: 20/08/2018                 --
+-- Autor(es) ..............: João Pedro Sconetto        --
+-- Banco de Dados .........: MySQL                      --
+-- Base de Dados (nome) ...: aula1exer2                 --
+--                                                      --
+-- PROJETO => 1 Base de Dados                           --
+--         => 10 Tabelas                                --
+--                                                      --
+-- ---------------------------------------------------- --
+
+CREATE DATABASE IF NOT EXISTS aula1exer2;
+
+USE aula1exer2;
 
 CREATE TABLE Funcionario (
     nome varchar(200),
@@ -75,52 +91,52 @@ CREATE TABLE Supervisiona (
     FK_Gerente_FK_Funcionario_cpf bigint,
     area_empregado varchar(100)
 );
- 
+
 ALTER TABLE Gerente ADD CONSTRAINT FK_Gerente_1
     FOREIGN KEY (FK_Funcionario_cpf)
     REFERENCES Funcionario (cpf)
     ON DELETE CASCADE ON UPDATE CASCADE;
- 
+
 ALTER TABLE Empregado ADD CONSTRAINT FK_Empregado_1
     FOREIGN KEY (FK_numeros_telefone_numeros_telefone_PK)
     REFERENCES numeros_telefone (numeros_telefone_PK)
     ON DELETE NO ACTION ON UPDATE NO ACTION;
- 
+
 ALTER TABLE Empregado ADD CONSTRAINT FK_Empregado_2
     FOREIGN KEY (FK_Funcionario_cpf)
     REFERENCES Funcionario (cpf)
     ON DELETE CASCADE ON UPDATE CASCADE;
- 
+
 ALTER TABLE Empregado ADD CONSTRAINT FK_Empregado_3
     FOREIGN KEY (FK_Endereco_id)
     REFERENCES Endereco (id)
     ON DELETE RESTRICT ON UPDATE RESTRICT;
- 
+
 ALTER TABLE Realiza ADD CONSTRAINT FK_Realiza_0
     FOREIGN KEY (FK_Venda_data_venda, FK_Venda_id)
     REFERENCES Venda (data_venda, id)
     ON DELETE RESTRICT ON UPDATE RESTRICT;
- 
+
 ALTER TABLE Realiza ADD CONSTRAINT FK_Realiza_1
     FOREIGN KEY (FK_Empregado_matricula, FK_Empregado_FK_Funcionario_cpf)
     REFERENCES Empregado (matricula, FK_Funcionario_cpf)
     ON DELETE RESTRICT ON UPDATE RESTRICT;
- 
+
 ALTER TABLE Possui ADD CONSTRAINT FK_Possui_0
     FOREIGN KEY (FK_Produto_id)
     REFERENCES Produto (id)
     ON DELETE RESTRICT ON UPDATE RESTRICT;
- 
+
 ALTER TABLE Possui ADD CONSTRAINT FK_Possui_1
     FOREIGN KEY (FK_Venda_data_venda, FK_Venda_id)
     REFERENCES Venda (data_venda, id)
     ON DELETE RESTRICT ON UPDATE RESTRICT;
- 
+
 ALTER TABLE Supervisiona ADD CONSTRAINT FK_Supervisiona_0
     FOREIGN KEY (FK_Empregado_matricula, FK_Empregado_FK_Funcionario_cpf)
     REFERENCES Empregado (matricula, FK_Funcionario_cpf)
     ON DELETE RESTRICT ON UPDATE RESTRICT;
- 
+
 ALTER TABLE Supervisiona ADD CONSTRAINT FK_Supervisiona_1
     FOREIGN KEY (FK_Gerente_email, FK_Gerente_FK_Funcionario_cpf)
     REFERENCES Gerente (email, FK_Funcionario_cpf)
